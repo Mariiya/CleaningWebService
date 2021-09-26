9733f5c07ad4aa9fa1ef9f0d3fa880201cbbf2d1
Byelyayeva <l.byelyayeva@gmail.com>
2021-09-27 01:49:34 +0300

    add Consumer

--- src/main/java/com/opsu/models/Vendor.java
+++ src/main/java/com/opsu/models/Vendor.java
@@ -1,4 +1,39 @@
 package com.opsu.models;
 
+import com.opsu.models.enumeration.Role;
+
+import java.math.BigInteger;
+
 public class Vendor {
+
+private  String firstName ;
+private  String lastName ;
+private  Boolean individual;
+
+
+public String  getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName= firstName;
+    }
+
+    public String  getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName= lastName;
+    }
+
+    public Boolean getIndividual() {
+        return individual;
+    }
+    public void setIndividual(boolean individual) {
+        this.individual= individual;
+    }
+    public Vendor (String firstName , String lastName , Boolean individual ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.individual = individual;
+    }
 }
