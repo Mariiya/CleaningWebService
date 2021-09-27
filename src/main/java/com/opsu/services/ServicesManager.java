a53022ba2daf49f8b6e92d33875a4b528354113b
Byelyayeva <l.byelyayeva@gmail.com>
2021-09-28 01:57:48 +0300

    add ServicesManager

--- src/main/java/com/opsu/services/ServicesManager.java
+++ src/main/java/com/opsu/services/ServicesManager.java
@@ -1,4 +1,27 @@
 package com.opsu.services;
 
-public class ServicesManager {
+import com.opsu.dao.ServiceDao;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ServicesManager {private final ServiceDao serviceDao;
+
+    @Autowired
+    public ServicesManager (ServiceDao serviceDao) {
+        this.serviceDao=serviceDao;
+    }
+
+
+    public void create(ServiceDao serviceDao) {
+
+    }
+    public void change(ServiceDao serviceDao) {
+
+    }
+    public void delete(ServiceDao serviceDao) {
+
+    }
+
+    public void addNew(ServiceDao serviceDao) {
+
+    }
 }
