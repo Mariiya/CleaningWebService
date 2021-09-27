3572f9ec83b29a96f0190b65c71d9ae86c9ed87a
Byelyayeva <l.byelyayeva@gmail.com>
2021-09-28 01:25:07 +0300

    add ServicesManager

---
+++ src/main/java/com/opsu/services/ConsumerService.java
@@ -0,0 +1,22 @@
+package com.opsu.services;
+
+import com.opsu.dao.ConsumerDao;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ConsumerService {private final ConsumerDao consumerDao;
+
+    @Autowired
+    public ConsumerService (ConsumerDao consumerDao) {
+        this.consumerDao = consumerDao;
+    }
+
+
+    public void create(ConsumerDao consumerDao) {
+
+    }
+    public void update(ConsumerDao consumerDao) {
+
+    }
+
+
+}
