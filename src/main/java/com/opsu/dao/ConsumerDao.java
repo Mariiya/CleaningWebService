539848b51bc7490aea2b2c0bbe213eb3f1eb1ccb
Mariiya <mariakozusan@gmail.com>
2021-11-10 10:11:49 +0200

    Vendor and Consumer get method fix

--- src/main/java/com/opsu/dao/ConsumerDao.java
+++ src/main/java/com/opsu/dao/ConsumerDao.java
@@ -13,7 +13,7 @@ public interface ConsumerDao {
     Consumer findConsumerByLastName(String lastName) throws NotFoundException;
     void save (Consumer consumer);
     String GET_CONSUMER_BY_ID = "SELECT  firstName, lastName, consumer.userId," +
-            " users.userId, users.email, users.password, users.phoneNumber, users.role " +
+            "  users.email, users.password, users.phoneNumber, users.role " +
             " FROM CONSUMER" +
             " LEFT JOIN users ON consumer.userId = users.userId" +
             " WHERE consumer.userId = ?";
